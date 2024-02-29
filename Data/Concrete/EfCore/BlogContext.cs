f17170f26c2d3632fded2f224e255f04d6d85212
Muhammed Hatay <46846228+trappigs@users.noreply.github.com>
2024-02-29 15:20:17 +0300

    DbContext/86

---
+++ Data/Concrete/EfCore/BlogContext.cs
@@ -0,0 +1,20 @@
+﻿using BlogApp.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Data.Concrete.EfCore
+{
+    public class BlogContext: DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options): base(options)
+        {
+            
+        }
+
+        // bununla
+        public DbSet<Post> Posts => Set<Post>();
+        // bunun arasında fark yok
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
